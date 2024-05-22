914053a65beae8acb08e03f1425a5742ed810f4d
PaquierThomas <thomas.paquier@edu.univ-fcomte.fr>
2024-05-22 17:27:33 +0200

    commit done card thematic + ajout payload et boucle card

--- cms/src/collections/Medias.ts
+++ cms/src/collections/Medias.ts
@@ -1,13 +1,96 @@
 import { CollectionConfig } from "payload/types";
 
-const Medias: CollectionConfig = {
+export const Medias: CollectionConfig = {
   slug: "medias",
+  access: {
+    read: () => true,
+  },
+  upload: {
+    staticURL: "/medias",
+    staticDir: "medias",
+    imageSizes: [
+      {
+        name: "thumbnail",
+        width: 400,
+        height: 300,
+        position: "centre",
+      },
+      {
+        name: "card",
+        width: 768,
+        height: 1024,
+        position: "centre",
+      },
+      {
+        name: "tablet",
+        width: 1024,
+        // By specifying `undefined` or leaving a height undefined,
+        // the image will be sized to a certain width,
+        // but it will retain its original aspect ratio
+        // and calculate a height automatically.
+        height: undefined,
+        position: "centre",
+      },
+    ],
+    adminThumbnail: "thumbnail",
+    mimeTypes: ["image/*"],
+  },
   fields: [
     {
-      name: "url",
-      label: "Url",
+      name: "alt",
+      label: "Alt",
       type: "text",
-      required: true,
+      required: false,
+    },
+    {
+      name: "legend",
+      label: "Legend",
+      type: "text",
+      required: false,
+    },
+    {
+      name: "title",
+      label: "Title",
+      type: "text",
+      required: false,
+    },
+    {
+      name: "description",
+      label: "Description",
+      type: "text",
+      required: false,
+    },
+    {
+      name: "credits",
+      label: "Credits",
+      type: "text",
+      required: false,
+    },
+    {
+      name: "date",
+      label: "Date",
+      type: "text",
+      required: false,
+    },
+    {
+      name: "type",
+      label: "Type",
+      type: "select",
+      options: [
+        {
+          label: "Image",
+          value: "image",
+        },
+        {
+          label: "Video",
+          value: "video",
+        },
+        {
+          label: "Audio",
+          value: "audio",
+        },
+      ],
+      required: false,
     },
   ],
 };
--- cms/src/collections/Thematics.ts
+++ cms/src/collections/Thematics.ts
@@ -2,7 +2,16 @@ import { CollectionConfig } from "payload/types";
 
 const Thematics: CollectionConfig = {
   slug: "thematics",
+  access: {
+    read: () => true,
+  },
   fields: [
+    {
+      name: "backgroundImage", // required
+      type: "upload", // required
+      relationTo: "medias", // required
+      required: true,
+    },
     {
       name: "title",
       label: "Title",
