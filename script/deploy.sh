14a085c5cbd1b6b884d868af54e787905cddbf6a
PaquierThomas <thomas.paquier@edu.univ-fcomte.fr>
2024-06-04 23:44:05 +0200

    import serveur

---
+++ script/deploy.sh
@@ -0,0 +1,5 @@
+# docker build -t "gou-cms" cms
+# docker run -d -p 3000:3000 --name gou-cms gou-cms
+docker build -t "gou-web" web
+docker run -d -p 8080:4321 --name gou-web gou-web
+# Décommentez les lignes ci-dessus pour déployer le serveur de base de données
