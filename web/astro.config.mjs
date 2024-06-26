309484ca0e8c02ef109c107de98e73e76fa2f656
sebjacquot <sebastien.jacquot@univ-fcomte.fr>
2024-06-26 15:03:40 +0200

    Update astro.config.mjs / tests pour reverse proxy

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -5,4 +5,7 @@ import node from "@astrojs/node";
 export default defineConfig({
   output: "server",
   adapter: node({ mode: "standalone" }),
+  // tests for reverse proxy on fanum
+   site: 'https://fanum.univ-fcomte.fr',
+   base: '/memoires-ouvrieres-goux  
 });
