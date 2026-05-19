8805dfbfbed106ba9bd37b1e720d9474de60a713
sebjacquot <sebastien.jacquot@univ-fcomte.fr>
2026-05-19 19:15:33 +0200

    Add old experimental server actions in next.config
    May needed to fix DELETE PATCH HTTP requests

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -8,6 +8,28 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath,
+	
+	
+	
+/*
+était nécessaire sur l'ancienne machine, à voir si on en a encore besoin	
+	  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "localhost:3000",
+        "mogoux-dev-fanum.inframshe.univ-fcomte.fr",
+        "mogoux-dev-fanum.inframshe.univ-fcomte.fr:3000",
+        "fanum.univ-fcomte.fr",
+        "172.20.81.117"
+      ],
+      allowedForwardedHosts: [
+        "fanum.univ-fcomte.fr",
+        "172.20.81.117"
+      ],
+    }
+  },
+  */
+  
   // Permet de servir les médias uploadés dans PayloadCMS
   images: {
     remotePatterns: [
