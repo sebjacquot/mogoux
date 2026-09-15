9828666671f3375bc07750e5c4143a1cc0de26da
ethanvuillemin <116081783+ethanvuillemin@users.noreply.github.com>
2026-09-15 03:38:49 +0200

    chore: active la vérification TypeScript au build

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -8,10 +8,6 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 		
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
   basePath,
 
   // Permet de servir les médias uploadés dans PayloadCMS
